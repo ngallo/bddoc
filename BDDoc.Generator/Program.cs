8bebdc93d1f1f3f8770ecb266539ff3c1c1ced80
Nicola Gallo <nicolagmt@hotmail.com>
2014-11-09 20:37:07 +0000

    Implemented BDDocGenerator MSBUILD target

--- BDDoc.Generator/Program.cs
+++ BDDoc.Generator/Program.cs
@@ -1,10 +1,29 @@
-﻿
+﻿using System;
+using System.Linq;
+
 namespace BDDoc.Generator
 {
     class Program
     {
+        private const string CBDDocFolderArgName = "/BDDocFolder:";
+
         static void Main(string[] args)
         {
+            Console.WriteLine(args.FirstOrDefault());
+            var bddocFolderArg = args.FirstOrDefault(s => s.ToUpper().StartsWith(CBDDocFolderArgName.ToUpper()));
+            if (string.IsNullOrWhiteSpace(bddocFolderArg))
+            {
+                Console.WriteLine("Invalid arguments.");
+                Environment.Exit(1);
+            }
+            var bddocFolder = bddocFolderArg.Remove(0, CBDDocFolderArgName.Length);
+            if (string.IsNullOrWhiteSpace(bddocFolder))
+            {
+                Console.WriteLine("Invalid BDDocFolder argument.");
+                Console.WriteLine("Error");
+                Environment.Exit(1);
+            }
+            Environment.Exit(0);
         }
     }
 }
